8b046708b200767a3b3a38c193bf273cf4d99db6
arthur <arthur.rios007@gmail.com>
2024-12-28 00:23:37 -0300

    feat: add token to cookies

--- apps/web/src/app/auth/sign-in/actions.tsx
+++ apps/web/src/app/auth/sign-in/actions.tsx
@@ -1,6 +1,8 @@
 'use server'
 
 import { HTTPError } from 'ky'
+import { cookies } from 'next/headers'
+import { redirect } from 'next/navigation'
 import { z } from 'zod'
 
 import { signInWithPassword } from '@/http/sign-in-with-password'
@@ -28,7 +30,13 @@ export async function signInWithEmailAndPassword(data: FormData) {
       email,
       password,
     })
-    console.log(token)
+
+    const cookieStore = await cookies()
+
+    cookieStore.set('token', token, {
+      path: '/',
+      maxAge: 60 * 60 * 24 * 7, // 7 days
+    })
   } catch (err) {
     if (err instanceof HTTPError) {
       const { message } = await err.response?.json()
@@ -45,5 +53,5 @@ export async function signInWithEmailAndPassword(data: FormData) {
     }
   }
 
-  return { success: true, message: null, errors: null }
+  redirect('/')
 }
