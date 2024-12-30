4993ab61983aa6884ce2b4265776260f8837ca3e
arthur <arthur.rios007@gmail.com>
2024-12-30 15:52:08 -0300

    feat: update member role

--- apps/web/src/app/(app)/org/[slug]/members/actions.ts
+++ apps/web/src/app/(app)/org/[slug]/members/actions.ts
@@ -1,12 +1,25 @@
 'use server'
 
+import type { Role } from '@saas/auth'
+
 import { getCurrentOrg } from '@/auth/auth'
 import { removeMember } from '@/http/remove-member'
+import { updateMember } from '@/http/update-member'
 
 export async function removeMemberAction(memberId: string) {
   const currentOrg = await getCurrentOrg()
 
   await removeMember({
+    org: currentOrg!,
+    memberId,
+  })
+}
+
+export async function updateMemberAction(memberId: string, role: Role) {
+  const currentOrg = await getCurrentOrg()
+
+  await updateMember({
+    role,
     org: currentOrg!,
     memberId,
   })
