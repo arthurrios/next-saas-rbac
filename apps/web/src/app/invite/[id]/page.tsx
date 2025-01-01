967e3324e0b781f9f6d028fa740dc534758b8343
arthur <arthur.rios007@gmail.com>
2025-01-01 15:54:49 -0300

    fix: async types in invite page

--- apps/web/src/app/invite/[id]/page.tsx
+++ apps/web/src/app/invite/[id]/page.tsx
@@ -15,13 +15,11 @@ import { getInvite } from '@/http/get-invite'
 dayjs.extend(relativeTime)
 
 interface InvitePageProps {
-  params: {
-    id: string
-  }
+  params: Promise<{ id: string }>
 }
 
 export default async function InvitePage({ params }: InvitePageProps) {
-  const inviteId = params.id
+  const { id: inviteId } = await params
 
   const { invite } = await getInvite(inviteId)
   const isUserAuthenticated = await isAuthenticated()
